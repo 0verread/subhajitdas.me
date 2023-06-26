a0654d7308142c8a9c7557f7bea41bf63a759d4c
sub0ne1 <iamsubhajit.d@gmail.com>
2023-06-26 16:32:38 -0400

    Home Page: Done

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import blogs from '../utils/blogs.json';
 
 const Blogs:React.FC = () => {
+
   return (
-    <div className='p-2'>Blogs</div>
+    <div className='p-2'>
+      <ul className='"space-y-4 list-disc list-inside dark:text-gray-400"'>
+      {blogs.blogs.map((blog: any, i: any) => (
+        <li key={i}>{blog.name} {blog.date}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
