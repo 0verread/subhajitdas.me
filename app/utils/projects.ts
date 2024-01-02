4bb43bcca7eba96e8ebda219a7aee33717cd328c
sub0ne1 <iamsubhajit.d@gmail.com>
2024-01-02 14:24:15 -0500

    started

---
+++ app/utils/projects.ts
@@ -0,0 +1,9 @@
+const allProjects  = [
+  {
+    "name": "Goose",
+    "description": "", //(optional) short description/one liner
+    "link": "", // github/application link 
+  }
+]
+
+export default allProjects;
