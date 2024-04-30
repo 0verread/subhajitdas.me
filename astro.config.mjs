3e19ed94b9f223c988b986c8191610e44f13cb68
sub0ne1 <iamsubhajit.d@gmail.com>
2024-04-30 01:17:00 -0400

    wrote stupid code: suffering now

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
