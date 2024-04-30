2a16fd3e882cf48712e0e2d8976b92dadd17c66e
sub0ne1 <iamsubhajit.d@gmail.com>
2024-04-30 01:11:35 -0400

    added vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [
     tailwind(),
     mdx({ syntaxHighlight: "shiki", shikiConfig: { theme: "dracula" } }),
