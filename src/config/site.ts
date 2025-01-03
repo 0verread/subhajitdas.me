cf07af78fc3bfbcc0ba5b48a77e5ea51f5ace368
sub0ne1 <iamsubhajit.d@gmail.com>
2025-01-03 14:03:21 -0500

    mt-4 in layout

---
+++ src/config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+    title: "Subhajit Das",
+    description: "Subhajit Das's personal website",
+    url: "https://subhajitdas.com",
+    author: "Subhajit Das",
+    twitter: "https://x.com/subhajitsh",
+    linkedin: "https://www.linkedin.com/in/subhajitsh/",
+    github: "https://github.com/0verread",
+}
